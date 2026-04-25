a19b58e2919b6a194f71a5a0cd268dd5622b8266
Sanskar Kumar <kumarsanskarjha12@bbdec.ac.in>
2026-04-25 11:58:16 +0530

    added whileloop

---
+++ Learn-the-basics/Things-to-know-in-Java/Whileloops.java
@@ -0,0 +1,17 @@
+public class Whileloops {
+    public static int whileLoop(int d) {
+        int numDigit = d;
+        int sum = 0;
+        for (int i = 1; i <= 50; i++) {
+            sum += numDigit;
+            numDigit += 10;
+        }
+
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(whileLoop(1));
+
+
+    }
+}
