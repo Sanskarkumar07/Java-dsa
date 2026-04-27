d2a3ea34504a0e4a45077cefa7fbb1cd308c7202
Sanskar Kumar <kumarsanskarjha12@bbdec.ac.in>
2026-04-27 19:54:10 +0530

    patterns

---
+++ Learn-the-basics/Build-up Logical Thinking/Patterns.java
@@ -0,0 +1,65 @@
+public class Patterns {
+
+    public static void print1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+    }
+    public static void print2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void print3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void print4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void print5(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void print6(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        
+    }
+    public static void main(String[] args) {
+
+        // print1(5);
+        // print2(5);
+        // print3(5);
+        // print4(5);
+        // print5(5);
+        // print6(5);
+
+       
+    }
+
+}
