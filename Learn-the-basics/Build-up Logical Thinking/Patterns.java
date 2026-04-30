f0be36a41f2ace35b271182a0d8cff717142e6d2
Sanskar Kumar <kumarsanskarjha12@bbdec.ac.in>
2026-05-01 00:58:57 +0530

    pattern 8

--- Learn-the-basics/Build-up Logical Thinking/Patterns.java
+++ Learn-the-basics/Build-up Logical Thinking/Patterns.java
@@ -63,6 +63,17 @@ public class Patterns {
             System.out.println();
         }
     }
+    public static void print8(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * n - 1) - (2 * i); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+    }
     public static void main(String[] args) {
 
     // print1(5);
@@ -71,7 +82,8 @@ public class Patterns {
     // print4(5);
     // print5(5);
     // print6(5);
-    print7(5);
+    // print7(5);
+    print8(5);
 }
 
 }
